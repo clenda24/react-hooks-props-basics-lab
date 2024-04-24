1d75c9f4772cdfe3471dad14f9c5bf3d5c7d5956
clenda <glendabittok24@gmail.com>
2024-04-24 19:23:22 +0300

    complete lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function Home(props) {
+  
   return (
     <div id="home">
       <h1 style={{ color: props.color }}>
@@ -9,5 +10,7 @@ function Home(props) {
     </div>
   );
 }
+
+
 
 export default Home;
